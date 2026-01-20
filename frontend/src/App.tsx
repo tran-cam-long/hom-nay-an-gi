066c666101d85a10e9a07fcda725a45c2f0f970b
Long Tran <camlong.tran0702@gmail.com>
2026-01-20 17:21:42 +0700

    [US2] feature: add image and style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import bodyBackground from './assets/conmeo_background.webp'
 import './App.css'
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
   return (
     <>
       <div>
+        <div className='bodyImage'>
+          <img src={bodyBackground} alt='Background cats' />
+        </div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +20,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello conmeo Vien</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
