6d6589e578e02518386eb6439742b51855b08820
tran-cam-long <camlong.tran070@gmail.com>
2026-02-16 18:50:55 +0700

    [US12] feature: init game server types and store

--- server/src/multiplayer/multiplayer.module.ts
+++ server/src/multiplayer/multiplayer.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MultiplayerGateway } from './multiplayer.gateway';
+import { MultiplayerStore } from './services/multiplayer.store';
 
 @Module({
-  providers: [MultiplayerGateway],
+  providers: [MultiplayerGateway, MultiplayerStore],
+  exports: [MultiplayerStore],
 })
-export class MultiplayerModule {}
+export class MultiplayerModule { }
